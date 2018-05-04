b66861b91e5cce5e2c2cb88638d2616b638a348e
Jannis B <jannis.bolik@student.hpi.de>
2018-05-04 20:31:22 +0200

    Seperated player code from Entity script

--- HeadsUp/Assets/Scripts/Entities/Entity.cs
+++ HeadsUp/Assets/Scripts/Entities/Entity.cs
@@ -9,50 +9,23 @@ namespace Entities
 		public Vector2 LocalGravity;
         public GameObject ground_hit_effect;
 
-        private CircleCollider2D _circ_col;
-		private Rigidbody2D _rigidbody;
-        private InputController input;
-        private int health;
-        private float hit_knockback = 10;
+        
+		protected Rigidbody2D _rigidbody;
+        
 	
 		// Use this for initialization
-		void Start ()
+		protected virtual void Start ()
 		{
-            health = 5;
-            _circ_col = GetComponent<CircleCollider2D>();
 			_rigidbody = GetComponent<Rigidbody2D>();
-            input = GetComponent<InputController>();
-		}
-	
-		// Update is called once per frame
-		void Update () {
-			
 		}
 
-		private void FixedUpdate()
+		protected virtual void FixedUpdate()
 		{
 			transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.down, LocalGravity));
 			_rigidbody.AddForce(LocalGravity, ForceMode2D.Force);
-			var movement = new Vector2(-LocalGravity.normalized.y, LocalGravity.normalized.x);
-			var targetForce = input.Horizontal * 5.0f;
-			var currentHorizontal = new Vector2(_rigidbody.velocity.x * movement.x * movement.x, _rigidbody.velocity.y * movement.y * movement.y);
-			var targetHorizontal = movement * targetForce;
-			var delta = (targetHorizontal - currentHorizontal).normalized;
-
-			if (input.Jump)
-			{
-				var currentVertical = new Vector2(_rigidbody.velocity.x * LocalGravity.normalized.x * LocalGravity.normalized.x, _rigidbody.velocity.y * LocalGravity.normalized.y * LocalGravity.normalized.y);
-				_rigidbody.AddForce(-currentVertical, ForceMode2D.Impulse);
-				_rigidbody.AddForce(LocalGravity.normalized * -16.0f, ForceMode2D.Impulse);
-			}
-			//Debug.DrawLine(transform.position, transform.position + new Vector3((movement * delta).x, (movement * delta).y, 0), Color.green);
-			Debug.DrawLine(transform.position, transform.position + new Vector3(currentHorizontal.x, currentHorizontal.y, 0), Color.green);
-			Debug.DrawLine(transform.position, transform.position + new Vector3(targetHorizontal.x, targetHorizontal.y, 0), Color.red);
-			Debug.DrawLine(transform.position, transform.position + new Vector3(movement.x, movement.y, 0), Color.yellow);
-			_rigidbody.AddForce(delta * 150.0f, ForceMode2D.Force);
 		}
 
-		private void OnCollisionEnter2D(Collision2D other)
+		protected virtual void OnCollisionEnter2D(Collision2D other)
 		{
 			if (other.gameObject.CompareTag("Wall"))
 			{
@@ -63,16 +36,6 @@ namespace Entities
 					OnGroundTouch();
 				}
 			}
-            //Braucht noch nen CircleCollider für den Kopf
-            if (other.otherCollider == _circ_col)
-            {
-                health--;
-                _rigidbody.AddForce(-transform.up.normalized * hit_knockback, ForceMode2D.Impulse);
-                if (health <= 0)
-                {
-                    //Die
-                }
-            }
 		}
 
 		private void OnGroundTouch()
--- HeadsUp/Assets/Scripts/Entities/Player.cs
+++ HeadsUp/Assets/Scripts/Entities/Player.cs
@@ -1,7 +1,58 @@
-﻿namespace Entities
+﻿using UnityEngine;
+
+namespace Entities
 {
     public class Player : Entity
     {
-        
+        private InputController input;
+
+        private CircleCollider2D _circ_col;
+        private int health = 5;
+        private float hit_knockback = 10;
+
+        protected override void Start()
+        {
+            base.Start();
+            _circ_col = GetComponent<CircleCollider2D>();
+            input = GetComponent<InputController>();
+
+        }
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            var movement = new Vector2(-LocalGravity.normalized.y, LocalGravity.normalized.x);
+            var targetForce = input.Horizontal * 5.0f;
+            var currentHorizontal = new Vector2(_rigidbody.velocity.x * movement.x * movement.x, _rigidbody.velocity.y * movement.y * movement.y);
+            var targetHorizontal = movement * targetForce;
+            var delta = (targetHorizontal - currentHorizontal).normalized;
+
+            if (input.Jump)
+            {
+                var currentVertical = new Vector2(_rigidbody.velocity.x * LocalGravity.normalized.x * LocalGravity.normalized.x, _rigidbody.velocity.y * LocalGravity.normalized.y * LocalGravity.normalized.y);
+                _rigidbody.AddForce(-currentVertical, ForceMode2D.Impulse);
+                _rigidbody.AddForce(LocalGravity.normalized * -16.0f, ForceMode2D.Impulse);
+            }
+            //Debug.DrawLine(transform.position, transform.position + new Vector3((movement * delta).x, (movement * delta).y, 0), Color.green);
+            Debug.DrawLine(transform.position, transform.position + new Vector3(currentHorizontal.x, currentHorizontal.y, 0), Color.green);
+            Debug.DrawLine(transform.position, transform.position + new Vector3(targetHorizontal.x, targetHorizontal.y, 0), Color.red);
+            Debug.DrawLine(transform.position, transform.position + new Vector3(movement.x, movement.y, 0), Color.yellow);
+            _rigidbody.AddForce(delta * 150.0f, ForceMode2D.Force);
+        }
+
+        protected override void OnCollisionEnter2D(Collision2D other)
+        {
+            base.OnCollisionEnter2D(other);
+            //Braucht noch nen CircleCollider für den Kopf
+            if (other.otherCollider == _circ_col)
+            {
+                health--;
+                _rigidbody.AddForce(-transform.up.normalized * hit_knockback, ForceMode2D.Impulse);
+                if (health <= 0)
+                {
+                    //Die
+                }
+            }
+        }
     }
 }

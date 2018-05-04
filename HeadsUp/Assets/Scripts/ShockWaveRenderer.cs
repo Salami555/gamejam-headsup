87c150e5c340b5defe420fcf6ee9904f9a8d4352
Erik Brendel <erik.brendel@gmx.de>
2018-05-04 17:51:42 +0200

    Add click shockwaves

--- HeadsUp/Assets/Scripts/ShockWaveRenderer.cs
+++ HeadsUp/Assets/Scripts/ShockWaveRenderer.cs
@@ -1,51 +1,55 @@
 ﻿using System;
 using UnityEngine;
 
-namespace Postprocessing
+public class ShockWaveRenderer : MonoBehaviour
 {
-	public class ShockWaveRenderer : MonoBehaviour
-	{
+    public Material Mat;
+    public float speed;
 
-		public Material Mat;
-		public float speed;
-	
-		private Vector4[] hitPoints = new Vector4[10];
-		private int hitPointHead = 0;
+    private Vector4[] hitPoints = new Vector4[10];
+    private int hitPointHead = 0;
 
-		private void OnRenderImage(RenderTexture src, RenderTexture dest)
-		{
-			Graphics.Blit(src, dest, Mat);
-		}
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, Mat);
+    }
 
-		private void Start()
-		{
-			for (var i = 0; i < hitPoints.Length; i++)
-			{
-				hitPoints[i] = new Vector4(0, 0, 999999, 0);
-			}
-			Mat.SetVectorArray("_HitPoints", hitPoints);
-		}
+    private void Start()
+    {
+        for (var i = 0; i < hitPoints.Length; i++)
+        {
+            hitPoints[i] = new Vector4(0, 0, 999999, 0);
+        }
 
-		private void Update()
-		{
-			for (var i = 0; i < hitPoints.Length; i++)
-			{
-				hitPoints[i].z += Time.deltaTime * speed;
-			}
-		
-			Mat.SetVectorArray("_HitPoints", hitPoints);
-		}
+        Mat.SetVectorArray("_HitPoints", hitPoints);
+    }
 
-		public void MakeWave(Vector2 worldPosition, float strength = 1.0f)
-		{
-			Vector2 viewportSpace = Camera.main.WorldToViewportPoint(worldPosition);
-			hitPoints[hitPointHead].x = viewportSpace.x;
-			hitPoints[hitPointHead].y = viewportSpace.y;
-			hitPoints[hitPointHead].z = 0;
-			hitPoints[hitPointHead].w = strength;
-		
-			hitPointHead++;
-			if (hitPointHead >= hitPoints.Length) hitPointHead = 0;
-		}
-	}
+    private void Update()
+    {
+        for (var i = 0; i < hitPoints.Length; i++)
+        {
+            hitPoints[i].z += Time.deltaTime * speed;
+        }
+
+        Mat.SetVectorArray("_HitPoints", hitPoints);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            float radius = 4.0f;
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            MakeWave(pos);
+        }
+    }
+
+    public void MakeWave(Vector2 worldPosition, float strength = 1.0f)
+    {
+        Vector2 viewportSpace = Camera.main.WorldToViewportPoint(worldPosition);
+        hitPoints[hitPointHead].x = viewportSpace.x;
+        hitPoints[hitPointHead].y = viewportSpace.y;
+        hitPoints[hitPointHead].z = 0;
+        hitPoints[hitPointHead].w = strength;
+
+        hitPointHead++;
+        if (hitPointHead >= hitPoints.Length) hitPointHead = 0;
+    }
 }

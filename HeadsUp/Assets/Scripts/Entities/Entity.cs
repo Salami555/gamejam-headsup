25597565fb770063fbdff0931f70c276d6c00bbb
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2018-05-04 17:40:49 +0200

    Moar physics

---
+++ HeadsUp/Assets/Scripts/Entities/Entity.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Entities
+{
+	public class Entity : MonoBehaviour
+	{
+
+		public Vector2 LocalGravity;
+
+		private Rigidbody2D _rigidbody;
+		
+	
+		// Use this for initialization
+		void Start ()
+		{
+			_rigidbody = GetComponent<Rigidbody2D>();
+		}
+	
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+		private void FixedUpdate()
+		{
+			_rigidbody.AddForce(LocalGravity, ForceMode2D.Force);
+			var movement = new Vector2(-LocalGravity.y, LocalGravity.x);
+			_rigidbody.AddForce(movement * Input.GetAxis("Horizontal") * 20.0f, ForceMode2D.Force);
+		}
+	}
+}

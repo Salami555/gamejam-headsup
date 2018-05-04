a0ac72310dc7be7b5508f1d0ae218b3f389c1fa5
Jannis B <jannis.bolik@student.hpi.de>
2018-05-04 18:19:58 +0200

    Added InputController

---
+++ HeadsUp/Assets/Scripts/InputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private int playerNumber;
+
+    private static List<InputController> controllers;
+    private static int playerCounter;
+
+    private const string horizontalAxis = "Horizontal";
+    private const string gravityTurnAxis = "GravityTurn";
+    private const string jumpButton = "Jump";
+
+    private void Awake()
+    {
+        this.playerNumber = playerCounter++;
+        controllers.Add(this);
+    }
+
+    public float Horizontal { get { return Input.GetAxis(horizontalAxis + playerNumber); } }
+    public float GravityTurn { get { return Input.GetAxis(gravityTurnAxis + playerNumber); } }
+    public bool Jump { get { return Input.GetButtonDown(jumpButton + playerNumber); } }
+}

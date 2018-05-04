c0b6b47c8ac3edd487ca2948512652d0b0de6412
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2018-05-04 18:15:03 +0200

    Player movement

--- HeadsUp/Assets/Scripts/Entities/Entity.cs
+++ HeadsUp/Assets/Scripts/Entities/Entity.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Entities
 {
@@ -23,9 +24,18 @@ namespace Entities
 
 		private void FixedUpdate()
 		{
+			transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector2.down, LocalGravity));
 			_rigidbody.AddForce(LocalGravity, ForceMode2D.Force);
-			var movement = new Vector2(-LocalGravity.y, LocalGravity.x);
-			_rigidbody.AddForce(movement * Input.GetAxis("Horizontal") * 20.0f, ForceMode2D.Force);
+			var movement = new Vector2(-LocalGravity.normalized.y, LocalGravity.normalized.x);
+			var targetForce = Input.GetAxis("Horizontal") * 5.0f;
+			var currentHorizontal = new Vector2(_rigidbody.velocity.x * movement.x * movement.x, _rigidbody.velocity.y * movement.y * movement.y);
+			var targetHorizontal = movement * targetForce;
+			var delta = (targetHorizontal - currentHorizontal).normalized;
+			//Debug.DrawLine(transform.position, transform.position + new Vector3((movement * delta).x, (movement * delta).y, 0), Color.green);
+			Debug.DrawLine(transform.position, transform.position + new Vector3(currentHorizontal.x, currentHorizontal.y, 0), Color.green);
+			Debug.DrawLine(transform.position, transform.position + new Vector3(targetHorizontal.x, targetHorizontal.y, 0), Color.red);
+			Debug.DrawLine(transform.position, transform.position + new Vector3(movement.x, movement.y, 0), Color.yellow);
+			_rigidbody.AddForce(delta * 15.0f, ForceMode2D.Force);
 		}
 	}
 }

2dc7c1d058e7eea35e374a47632dd75cb5f2b2ba
Georg Tennigkeit <georg.tennigkeit@student.hpi.de>
2018-05-05 13:04:11 +0200

    first work on some particles that appear when rotating

---
+++ HeadsUp/Assets/Scripts/TurnEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnEffect : MonoBehaviour {
+
+    public Quaternion toRotate; //hier die Rotation, um die rotiert werden soll
+
+    private float effect_duration = 0.3f; //sollte die Dauer der Particlesystems sein
+    //Dazu sollte man die Lebensdauer der Partikel beachten
+    
+    
+    private float creation_time;
+    private Quaternion start_rot;
+
+	void Start () {
+        creation_time = Time.time;
+        start_rot = transform.rotation;
+        Destroy(this.gameObject, 1f);
+	}
+	
+	void Update () {
+        //transform.rotation = Quaternion.Lerp(start_rot, , Time.time - creation_time);
+	}
+}

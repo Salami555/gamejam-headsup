e16d80c546dba94fd870d58a4addfdc7f85a3ba7
Jannis B <jannis.bolik@student.hpi.de>
2018-05-05 15:13:27 +0200

    Added player sounds

---
+++ HeadsUp/Assets/Scripts/Entities/PlayerSounds.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using Entities;
+
+public class PlayerSounds : MonoBehaviour
+{
+    public AudioSource output;
+    public AudioClip thrustingIgnite;
+    public AudioClip thrustingLooping;
+
+    private Player.ThrustState? currentThrust = null;
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    private void Update()
+    {
+        if (currentThrust != player.Thrust)
+        {
+            switch (player.Thrust)
+            {
+                case Player.ThrustState.FULL:
+                    output.volume = 1.0f;
+                    if (currentThrust == Player.ThrustState.ZERO)
+                    {
+                        StopCoroutine("Thrusting");
+                        StartCoroutine("Thrusting"); 
+                    }
+                    break;
+                case Player.ThrustState.HALF:
+                    output.volume = 0.5f;
+                    if (currentThrust == Player.ThrustState.ZERO)
+                    {
+                        StopCoroutine("Thrusting");
+                        StartCoroutine("Thrusting");
+                    }
+                    break;
+                case Player.ThrustState.ZERO:
+                    StopCoroutine("Thrusting");
+                    output.Pause();
+                    break;
+                default:
+                    break;
+            }
+            currentThrust = player.Thrust;
+        }
+
+    }
+
+    public IEnumerator Thrusting()
+    {
+        output.loop = false;
+        output.clip = thrustingIgnite;
+        output.Play();
+        yield return new WaitForSeconds(thrustingIgnite.length);
+        output.loop = true;
+        output.clip = thrustingLooping;
+        output.Play();
+    }
+}

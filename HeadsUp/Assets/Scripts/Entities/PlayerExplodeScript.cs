bc8c12f2d789034fea38648f908cfa076f0cf87b
Markus-PC\Markus <markus.brand@student.hpi.de>
2018-05-05 13:51:12 +0200

    Add Explosion script

---
+++ HeadsUp/Assets/Scripts/Entities/PlayerExplodeScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Entities;
+using UnityEngine;
+
+public class PlayerExplodeScript : MonoBehaviour
+{
+
+	private Vector2 _randomDirection;
+	
+	private Player _player;
+	// Use this for initialization
+	void Start ()
+	{
+		_player = transform.GetComponentInParent<Player>();
+		_randomDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_player.health <= 0)
+		{
+			transform.Translate(_randomDirection * Time.deltaTime * 20.0f);
+		}
+	}
+}
